00994f2fcc808f73613e2fe5bf3d188a88d140ae
junioryi <tedi6410@gmail.com>
2015-08-22 14:03:05 +0800

    basic model

--- station/views.py
+++ station/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Status
 
 # Create your views here.
+def index(request):
+    status_list = Status.objects.all()
+    context = {'status_list': status_list}
+    return render(request, 'station/index.html', context)
+
+def detail(request, status_id):
+    return HttpResponse("Status number %s" % status_id)
+
+
