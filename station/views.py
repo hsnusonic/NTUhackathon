42310da3a18ea0bf98c5c31a134084efc4198a35
junioryi <tedi6410@gmail.com>
2015-08-22 16:36:29 +0800

    show api data

--- station/views.py
+++ station/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import Status
+
+import urllib, json
 
 # Create your views here.
 def index(request):
@@ -12,4 +14,28 @@ def index(request):
 def detail(request, status_id):
     return HttpResponse("Status number %s" % status_id)
 
+def update(request):
+    url = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=190796c8-7c56-42e0-8068-39242b8ec927 "
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+    station_list = [ station for station in data['result']['results']]
 
+    # Remove all previous data
+    for station in Status.objects.all():
+        station.delete()
+
+    # Save the new one
+    for station in station_list:
+        s = Status(
+            update_date = station["update_date"].encode('utf-8'),
+            update_time = station["update_time"].encode('utf-8'),
+            qua_id    = station["qua_id"].encode('utf-8'),
+            name      = station["code_name"].encode('utf-8'),
+            longitude = station["longitude"].encode('utf-8'),
+            latitude  = station["latitude"].encode('utf-8'),
+            qua_cntu  = station["qua_cntu"].encode('utf-8'),
+            qua_cl    = station["qua_cl"].encode('utf-8'),
+            qua_ph    = station["qua_ph"].encode('utf-8')
+        )
+        s.save()
+    return redirect('index')
